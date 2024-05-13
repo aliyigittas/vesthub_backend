1c51e5d5deab3ed63e2b9f6336ee4580147957f1
Ali Yiğit Taş <48025526+aliyigittas@users.noreply.github.com>
2024-05-13 18:39:25 +0300

    babalar sozunu tutar

--- src/main/java/com/group16/vesthub/DataController.java
+++ src/main/java/com/group16/vesthub/DataController.java
@@ -1,23 +1,63 @@
 package com.group16.vesthub;
 
+import javax.sql.DataSource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
+
 public class DataController {
+    
+    private DatabaseAdapter dbAdapter;
+    public DataController(DataSource dataSource) {
+        dbAdapter = new DatabaseAdapter(dataSource);
+    }
+
 
     @PostMapping("/api/login")
-    public boolean receiveDataFromFrontend(@RequestBody String data) {
+    public boolean receiveDataFromSignIn(@RequestBody String data) {
 
-        //böyle gelir: {"name":"John","age":30,"city":"New York"}
         System.out.println("Received data from frontend: " + data); 
         
-        // Here you can add logic to handle the received data
-        //burada kullanıcının tüm verileri fronta gönderilebilir doğru giriş yapılırsa
         return true;
     }
+
+    @PostMapping("/api/register")
+    public boolean receiveDataFromSignUp(@RequestBody String data) {        
+        try 
+        {
+            // Initialize ObjectMapper
+            ObjectMapper mapper = new ObjectMapper();
+            
+            // Parse JSON string to User object
+            User user = mapper.readValue(data, User.class);
+
+            // Print received data
+            System.out.println("Received data from frontend: " + data);
+            if (dbAdapter.checkUserExists(user.getEmail())) 
+            {
+                return false;
+            }
+            //insert database
+            dbAdapter.insertUser(user.getName(), user.getSurname(), user.getEmail(), "", user.getPassword(), "", "", "", false);
+            
+            return true;
+        } 
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+            return false;
+        }
+      
+    }
+
+
 }
 
--- src/main/java/com/group16/vesthub/DatabaseAdapter.java
+++ src/main/java/com/group16/vesthub/DatabaseAdapter.java
@@ -35,4 +35,17 @@ public class DatabaseAdapter {
         Gson gson = new Gson();
         return gson.toJson(resultSet);
     }
+
+    public void insertUser(String name, String surname, String email, String phone, String password, String fullAddress, String city, String country, boolean status) 
+    {
+        jdbcTemplate.update("INSERT INTO users (name, surname, email, phone, password, fullAddress, city, country, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", name, surname, email, phone, password, fullAddress, city, country, status);
+    }
+
+    public boolean checkUserExists(String email) 
+    {
+        int count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM users WHERE email = ?", Integer.class, email);
+        return count > 0;
+    }
+
+
 }
